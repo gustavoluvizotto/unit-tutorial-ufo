12cbc202fb815e8538564d94ff911ce88e486e63
Gustavo Luvizotto Cesar <gustavoluvizotto@gmail.com>
2019-05-01 22:54:41 +0200

    On Step 7 of 9. Pickups added.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+        public GameObject player;
+        private Vector3 offset;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+                // transform is coming from the game object that this script is attached to.
+                offset = transform.position - player.transform.position;
+        }
+
+        // Update is called once per frame
+        // LateUpdate is called after Update (garanteed)
+        void LateUpdate()
+        {
+                transform.position = player.transform.position + offset;
+        }
+}
