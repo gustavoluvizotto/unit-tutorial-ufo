878060de1dc5b602543f42eac833f640bda98ecf
Gustavo Luvizotto Cesar <gustavoluvizotto@gmail.com>
2019-05-05 17:22:04 +0200

    Finished.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,14 +1,23 @@
 ﻿using System.Collections;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
         public float speed;
+        public Text scoreText;
+        public Text winText;
+
         private Rigidbody2D rb2d;
+        private const string pickUpTag = "PickUp";
+        private int score;
 
         void Start()
         {
                 rb2d = GetComponent<Rigidbody2D>();
+                score = 0;
+                winText.text = "";
+                setScoreText();
         }
 
         void FixedUpdate()
@@ -17,5 +26,23 @@ public class PlayerController : MonoBehaviour
                 float moveVertical = Input.GetAxis("Vertical");
                 Vector2 movement = new Vector2(moveHorizontal, moveVertical);
                 rb2d.AddForce(movement * speed);
+        }
+        void OnTriggerEnter2D(Collider2D other)
+        {
+                if (other.gameObject.CompareTag(pickUpTag))
+                {
+                        other.gameObject.SetActive(false);
+                        score += 10;
+                        setScoreText();
+                }
+        }
+
+        void setScoreText()
+        {
+                scoreText.text = "Score: " + score.ToString();
+                if (score >= 80)
+                {
+                        winText.text = "You won!";
+                }
         }
 }
